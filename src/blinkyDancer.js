e7a630070e8795f08493481af37e9fe738f820af
jahroost <claysmart@gmail.com>
2017-10-10 12:28:29 -0500

    Progress

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -1,77 +1,13 @@
-
-// var makeBlinkyDancer = function(top, left, timeBetweenSteps) {
-//   var blinkyDancer = makeDancer(top, left, timeBetweenSteps); //create new dancer
-
-//   // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
-//   // so we must keep a copy of the old version of this function
-
-//   var oldStep = blinkyDancer.step; //creates variable for step method
-
-//   blinkyDancer.step = function() {
-//     // call the old version of step at the beginning of any call to this new version of step
-//     oldStep();//recursion
-//     // toggle() is a jQuery method to show/hide the <span> tag.
-//     // See http://api.jquery.com/category/effects/ for this and
-//     // other effects you can use on a jQuery-wrapped html tag.
-//     blinkyDancer.$node.toggle(); //shows and !shows $node
-//   };
-
-//   return blinkyDancer; 
-// };
-
-
-//---------------------------------------------------------------------
-
-
-
 var makeBlinkyDancer = function(top, left, timeBetweenSteps) {
-  // in here prototype methods are available
   makeDancer.call(this, top, left, timeBetweenSteps);
-  // this.timeBetweenSteps = timeBetweenSteps
 };
 
 makeBlinkyDancer.prototype = Object.create(makeDancer.prototype);
 makeBlinkyDancer.prototype.constructor = makeBlinkyDancer;
 
 makeBlinkyDancer.prototype.step = function() {
-  // call the old version of step at the beginning of 
-  //any call to this new version of step
 
-
-  console.log(this.color);
   makeDancer.prototype.step.call(this);
 
-  // toggle() is a jQuery method to show/hide the <span> tag.
-  // See http://api.jquery.com/category/effects/ for this and
-  // other effects you can use on a jQuery-wrapped html tag.
-  this.$node.toggle();
+  // this.$node.toggle();
 };
-
-
-//figure out a way to call makeDancer step function, with context of makeblinkyDancer
-//this in line 45 has to be correctly
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
